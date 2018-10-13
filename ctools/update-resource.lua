4197ef84a3236110b52ba782fcae9d7dbfdea010
Gao Daren <mmnmbdc@gmail.com>
2018-10-13 14:28:25 +0800

    fix a miss

--- ctools/update-resource.lua
+++ ctools/update-resource.lua
@@ -3,5 +3,6 @@
 os.execute("git clone https://github.com/xiangsong-guan/trail-cloud")
 os.execute("rm -rf ../html")
 os.execute("mv ./trail-cloud/html ../ ")
+os.execute("rm -rf ./trail-cloud")
 
 os.exit(0)
